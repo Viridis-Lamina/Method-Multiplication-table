b10c5167b5573f89f979735962c8b08ca8d13b93
Ryan Clarke <ryanclarke999@gmail.com>
2023-10-10 10:46:51 +0100

    Add project files.

---
+++ Method Multiplication table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodMulti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+            Multiplication (number);
+
+        }
+
+        public static void Multiplication (int number) 
+        { 
+        
+            for (int M = 2; M < 11; M++)
+            {
+                int answer = number * M;
+                Console.WriteLine("{0,2} * {1,2} = {2,2}",M,number,answer);
+
+            }
+
+
+        }
+    }
+}
